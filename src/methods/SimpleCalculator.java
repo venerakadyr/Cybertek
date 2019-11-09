e2d07d8c444c0f92306dc8359258ad7086e6aaa7
venerakadyr <57569728+venerakadyr@users.noreply.github.com>
2019-11-09 14:41:32 -0600

    updates

--- src/methods/SimpleCalculator.java
+++ src/methods/SimpleCalculator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main( String [] args){
-
+   plus();
 
     }
 // Create a static method called "plus", its return is void and it gets no arguments.
@@ -18,6 +18,13 @@ public class SimpleCalculator {
 //2
 //result: 3
    public static void  plus( ){
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter first number");
+        int num1=input.nextInt();
+       System.out.println("enter second number");
+       int  num2=input.nextInt();
+       int result=num1+num2;
+       System.out.println("result:"+result);
 
 
 
